edbacd1190128ae5f462e4c7dd2a7cfd62750f28
Andre Emidio <andresjc2008@gmail.com>
2019-09-30 15:41:26 -0300

    Atualização

---
+++ main.py
@@ -0,0 +1,16 @@
+import getCamImage
+import senImageToAzure
+import drawAnormalySafety
+import senImageToEztask
+
+def main():
+    getCamImage
+    senImageToAzure
+    drawAnormalySafety
+    senImageToEztask
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
